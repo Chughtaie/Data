bf144ef5f9feda23f811484201701f5f904be9de
Chughtae <Chughtae@github.com>
2020-12-09 16:29:11 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,4 +1,5 @@
-#include "Header.h"
+//#include "Header.h"
+#include "Header1.h"
 
 int main()
 {
@@ -7,7 +8,7 @@ int main()
 	obj.Insert(5, obj.getRoot());	obj.R_H();	cout << "\n-------------------------------------\n";
 	obj.Insert(3, obj.getRoot());	obj.R_H();	cout << "\n-------------------------------------\n";
 	obj.Insert(4, obj.getRoot());	obj.R_H();	cout << "\n-------------------------------------\n";
-	obj.Insert(2, obj.getRoot());	obj.R_H();	cout << "\n-------------------------------------\n";
+	//obj.Insert(2, obj.getRoot());	obj.R_H();	cout << "\n-------------------------------------\n";
 	//obj.Insert(1, obj.getRoot());	obj.R_H();	cout << "\n-------------------------------------\n";
 
 	
