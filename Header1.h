f18a70fadebf4d454b5adaefb1258f8aafdd70ee
Chughtae <Chughtae@github.com>
2020-12-10 14:57:20 +0500

    Update Header1.h

--- Header1.h
+++ Header1.h
@@ -1,4 +1,3 @@
-//comment
 #include<iostream>
 #include<cstring>
 using namespace std;
@@ -26,8 +25,8 @@ public:
 
 	Node<T>* root;
 	int height;
-	 int lh = 0, rh = 0;
-	 int h = 0, a = 0;
+	int lh = 0, rh = 0;
+	int h = 0, a = 0;
 
 	BSTree() {
 
@@ -43,13 +42,13 @@ public:
 	int R_H() {
 		h = 0, a = 0;
 		Height(root);
-		cout << "\nRoot\t" << root->data << "\t Root Height = " << h << endl;	
+		cout << "\nRoot\t" << root->data << "\t Root Height = " << h << endl;
 		return 1;
 	}
 
 	int baanceFactor(Node<T>* r)
 	{
-		
+
 		lh = 0; rh = 0;
 		Node* templ = r->left;
 		Node* tempr = r->right;
@@ -77,18 +76,18 @@ public:
 	}
 
 	int balanceFactor(Node<T>* r) {
-		
+
 		if (!root)
 			return -1;
-		h = 0,a = 0;
+		h = 0, a = 0;
 		Height(r->left); lh = h;
-		h = 0,a = 0;
+		h = 0, a = 0;
 		Height(r->right); rh = h;
 
-		if (r->left ) lh++;
+		if (r->left) lh++;
 		if (r->right) rh++;
-		cout << "\nData = "<< r->data << "\tlh\t" << lh << "\trh\t" << rh << endl;
-		return (lh-rh);
+		cout << "\nData = " << r->data << "\tlh\t" << lh << "\trh\t" << rh << endl;
+		return (lh - rh);
 	}
 
 	void Height(Node<T>* temp)
@@ -119,47 +118,40 @@ public:
 		return;
 	}
 
+	void RR(Node<T>** temp) {
+		cout << "\tRR\n";
+		Node<T>* ch;					// 4-3-5
+		ch = (*temp)->left;
+		(*temp)->left = ch->right;
+		ch->right = (*temp);
+		(*temp) = ch;
+
+	}
+
+	void LL(Node<T>** temp) {
+		cout << "\tLL\n";
+		Node<T>* ch;
+		ch = (*temp)->right;
+		(*temp)->right = ch->left;
+		ch->left = (*temp);
+		(*temp) = ch;
+
+
+	}
 
 	void Left(Node<T>** temp) {
-	
-		cout << "\nZone 1\n";
-		if (balanceFactor((*temp)->left) < 0) // 5-4-3
-		{
-			Node<T>* ch = (*temp)->left;
-			(*temp)->left = (*temp)->left->right;
-			if((*temp)->left->left)
-				ch->right = (*temp)->left->left;
-			else 
-				ch->right = (*temp)->left->right;
-			(*temp)->left->left = ch;
-		}
+		cout << "\nLeft";
+		LL(&((*temp)->left));
+		RR(temp);
 
-		Node<T>* ch;					// 4-3-5
-		ch = (*temp);
-		(*temp) = (*temp)->left;
-		ch->left = (*temp)->left->left;
-		(*temp)->right = ch;	
 	}
 
 	void Right(Node<T>** temp) {
-	
-		cout << "\nZone 2\n";
-		if (balanceFactor((*temp)->right) > 0) // 5-4-3
-		{
-			Node<T>* ch = (*temp)->right;
-			(*temp)->right = (*temp)->right->left;
-			if((*temp)->right->right)
-				ch->left = (*temp)->right->right;
-			else
-				ch->left = (*temp)->right->left;
-			(*temp)->right->right = ch;
-		}
 
-		Node<T>* ch;					// 4-3-5
-		ch = (*temp);
-		(*temp) = (*temp)->right;
-		ch->right = (*temp)->left;
-		(*temp)->left = ch;
+		cout << "\nRight";
+		RR(&((*temp)->right));
+		LL(temp);
+
 
 	}
 
@@ -174,12 +166,13 @@ public:
 			return;
 		}
 
-		if ((*temp)->data == number->data)
+		else if ((*temp)->data == number->data)
 		{
 			cout << " \n Given number is already present in tree.\n";
 			return;
 		}
-		if ((*temp)->data > number->data)
+
+		else if ((*temp)->data > number->data)
 		{
 			if ((*temp)->left != NULL)
 			{
@@ -187,37 +180,42 @@ public:
 
 
 				if (balanceFactor(*temp) == 2) { //5-3-4
-					
-					Left(temp);
+
+					if (number->data < (*temp)->left->data) // 5-4-3
+						RR(temp);
+					else
+						Left(temp);
 				}
 
 				return;
 			}
 			else
 			{
-				cout << "\nEnter1\t" << number->data << endl;
+				cout << "\nEnter\t" << number->data << endl;
 				//(*temp)->left = new Node<T>;
 				(*temp)->left = number;
 				return;
 			}
 		}
 
-		if ((*temp)->data < number->data)
+		else if ((*temp)->data < number->data)
 		{
 			if ((*temp)->right != NULL)
 			{
 				Insert(number, &((*temp)->right));
 
 				if (balanceFactor(*temp) == -2) { //5-3-4
-					
-					Right(temp);
+					if (number->data > (*temp)->right->data) // 5-4-3
+						LL(temp);
+					else
+						Right(temp);
 
 				}
 				return;
 			}
 			else
 			{
-				cout << "\nEnter2\t" << number->data << endl;
+				cout << "\nEnter\t" << number->data << endl;
 				//(*temp)->right = new Node<T>;
 				(*temp)->right = number;
 				return;
@@ -246,7 +244,7 @@ public:
 		{
 			display(temp->right);
 		}
-		
+
 	}
 
 	bool retrieve(int d)
@@ -362,308 +360,3 @@ public:
 	}
 };
 
-
-
-
-/*s
-void Insert(T d)
-{
-	Node* temp = root;
-
-	if (root == NULL)
-	{
-		root = new Node;
-		cout << d << endl;
-		root->data = d;
-		temp = root;
-
-	}
-	else
-	{
-
-		while (temp)
-		{
-
-			if (d > temp->data)
-			{
-				if (temp->right == NULL)
-				{
-					temp->right = new Node;
-					cout << d << endl;
-					temp->right->data = d;
-					break;
-				}
-				else
-
-					temp = temp->right;
-			}
-			else
-			{
-
-				if (temp->left == NULL)
-				{
-					temp->left = new Node;
-					cout << d << endl;
-					temp->left->data = d;  break;
-				}
-				else
-					temp = temp->left;
-			}
-		}
-
-		if (balanceFactor(temp) == -2 && balanceFactor(temp->right) == -1)
-		{
-			Node* tempnode1;
-			Node* tempnode2;
-			tempnode1 = temp;
-			tempnode2 = tempnode1->right;
-
-			tempnode1->right = tempnode2->left;
-			tempnode2->left = tempnode1;
-			temp = tempnode2;
-		}
-		else if (balanceFactor(temp) == 2 && balanceFactor(temp->left) == 1)
-		{
-			Node* tempnode1;
-			Node* tempnode2;
-			tempnode1 = temp;
-			tempnode2 = tempnode1->left;
-
-			tempnode1->left = tempnode2->right;
-			tempnode2->right = tempnode1;
-
-			temp = tempnode2;
-		}
-		else if (balanceFactor(temp) == -2 && balanceFactor(temp->right) == 1)
-		{
-			Node* tempnode1;
-			Node* tempnode2;
-			Node* tempnode3;
-			tempnode1 = temp;
-			tempnode2 = tempnode1->right;
-			tempnode3 = tempnode1->right->left;
-
-			tempnode1->right = tempnode3->left;
-			tempnode2->left = tempnode3->right;
-			tempnode3->left = tempnode1;
-			tempnode3->right = tempnode2;
-
-			temp = tempnode3;
-		}
-		else if (balanceFactor(temp) == 2 && balanceFactor(temp->left) == -1)
-		{
-			Node* tempnode1;
-			Node* tempnode2;
-			Node* tempnode3;
-			tempnode1 = temp;
-			tempnode2 = tempnode1->left;
-			tempnode3 = tempnode1->left->right;
-
-			tempnode1->left = tempnode3->right;
-			tempnode2->right = tempnode3->left;
-			tempnode3->right = tempnode1;
-			tempnode3->left = tempnode2;
-
-			temp = tempnode3;
-		}
-
-	}
-}*/
-
-
-
-
-
-
-
-
-
-/*
-Node* LL_Rotate(Node* temp) {
-
-	Node* ch;
-	ch = temp->left;
-	temp->left = NULL;
-	ch->right = temp;
-	return ch;
-}
-Node* RR_Rotate(Node* temp) {
-
-	Node* ch;
-	ch = temp->right;
-	temp->right = NULL;
-	ch->left = temp;
-	return ch;
-}
-Node* LR_Rotate(Node* temp) {
-
-	Node* ch = temp->left;
-	temp->left = ch->right;
-	ch->right = NULL;
-	temp->left->left = ch;
-	//ch->left = ch->right;
-	//ch->right = NULL;
-
-	return LL_Rotate(temp);
-}
-Node* RL_Rotate(Node* temp) {
-
-	Node* ch = temp->right;
-	temp->right = ch->left;
-	ch->left = NULL;
-	temp->right->right = ch;
-	//ch->right = ch->left;
-	//ch->left = NULL;
-
-	return RR_Rotate(temp);
-}
-
-
-
-
-Node* Ball(Node *temp) {
-
-	//h = 0, a = 0; Height(temp->left);
-	//cout << endl << temp->data << "\theightleft" << h;// << balanceFactor(temp) << endl;
-	//h = 0, a = 0; Height(temp->right);
-	//cout << endl << temp->data << "\theightright" << h;// << balanceFactor(temp) << endl;
-
-	cout << "\ndata\t" << temp->data << "\t Bal \t" << balanceFactor(temp) << endl;
-	cout << &(temp)<< endl;
-	if (balanceFactor(temp) > 1) {
-		cout << "\nZone 1\n";
-		if (balanceFactor(temp->left) > 0)
-			if (temp == root) { temp = LL_Rotate(temp); root = temp;	cout << "\t1111\t" << temp->data;; }
-			else {
-				temp = LL_Rotate(temp); cout << "\t2222\t" << temp->data;
-
-				//Node* yo = root; while (balanceFactor(yo->left) < 2) { yo = yo->left;  }
-				//yo->left = RR_Rotate(temp);
-			}
-		else
-			if (temp == root) { temp = LR_Rotate(temp); root = temp;	cout << "\t3333\t" << temp->data;; }
-			else { temp = LR_Rotate(temp);								cout << "\t4444\t" << temp->data; }
-	}
-	else if (balanceFactor(temp) < -1) {
-		cout << "\nZone 2\n";
-		if (balanceFactor(temp->right) < 0)
-			if (temp == root) { temp = RR_Rotate(temp); root = temp; }
-			else temp = RR_Rotate(temp);
-		else
-			if (temp == root) { temp = RL_Rotate(temp);  root = temp; }
-			else temp = RL_Rotate(temp);
-
-	}
-	return temp;
-}
-
-*/
-
-
-
-
-
-
-
-
-
-
-
-/*
-void Insert(Node<T>* number, Node<T>** temp)
-{
-	cout << "\nEnter\n";
-	if (root == NULL)
-	{
-		//root = new Node<T>;
-
-		//(temp) = &number;
-		return;
-	}
-	cout << "\nEnd\n";
-	if ((*temp)->data == number->data)
-	{
-		cout << " \n Given number is already present in tree.\n";
-		return;
-	}
-	if ((*temp)->data > number->data)
-	{
-		if ((*temp)->left != NULL)
-		{
-			cout << "\n111\n";
-			Insert(number, &((*temp)->left));
-
-
-			//if (balanceFactor(*temp) == 200) { //5-3-4
-			//	cout << "\nZone 1\n";
-			//	if (balanceFactor((*temp)->left) < 0) // 5-4-3
-			//	{
-			//		Node<T>* ch = (*temp)->left;
-			//		(*temp)->left = (*temp)->left->right;
-			//		ch->right = NULL;
-			//		(*temp)->left->left = ch;
-			//	}
-			//
-			//
-			//	Node<T>* ch;					// 4-3-5
-			//	Node<T>* right;					// 4-3-5
-			//	ch = (*temp);
-			//	(*temp) = (*temp)->left;
-			//	ch->left = NULL;
-			//	right = (*temp)->right;
-			//	(*temp)->right = ch;
-			//
-			//	//if(right != NULL)
-			//	//Insert(right,(temp));
-			//}
-			return;
-		}
-		else
-		{
-			cout << "\n222\n";
-			//(*temp)->left = new Node<T>;
-			(*temp)->left = number;
-			return;
-		}
-	}
-
-	if ((*temp)->data < number->data)
-	{
-		if ((*temp)->right != NULL)
-		{
-			cout << "\n333\n";
-			Insert(number, &((*temp)->right));
-
-			//if (balanceFactor(*temp) == -200) { //5-3-4
-			//	cout << "\nZone 2\n";
-			//	if (balanceFactor((*temp)->right) > 0) // 5-4-3
-			//	{
-			//		Node<T>* ch = (*temp)->right;
-			//		(*temp)->right = (*temp)->right->left;
-			//		ch->left = NULL;
-			//		(*temp)->right->right = ch;
-			//	}
-
-
-			//	Node<T>* ch;					// 4-3-5
-			//	Node<T>* left;					// 4-3-5
-			//	ch = (*temp);
-			//	(*temp) = (*temp)->right;
-			//	ch->right = NULL;
-			//	left = (*temp)->left;
-			//	(*temp)->left = ch;
-			//	//if(left!=NULL)
-			//	//Insert(left,(temp));
-
-			//}
-			return;
-		}
-		else
-		{
-			cout << "\n444\n";
-			//(*temp)->right = new Node<T>;
-			(*temp)->right = number;
-			return;
-		}
-	}
-}*/
