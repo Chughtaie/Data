a94292f2b74735ad3c6a409547acd9445796f7ee
Chughtae <Chughtae@github.com>
2020-12-10 14:49:23 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,5 +1,5 @@
 //#include "Header.h"
-#include "Header1.h"
+#include "Header.h"
 
 int main()
 {
@@ -21,22 +21,17 @@ int main()
 		
 		obj.Insert(ch, &obj.root);	obj.R_H();	cout << "\n-------------------------------------\n"; obj.display(obj.root);
 	}
-	
-	for (int i=16;i>=12;i--)
+	for (int i = 16; i >= 8; i--)
 	{
+		if (i == 9) i--;
 		Node<int>* ch;
 		ch = new Node<int>;
 		ch->data = i;
 
 		obj.Insert(ch, &obj.root);	obj.R_H();	cout << "\n-------------------------------------\n"; obj.display(obj.root);
 	}
-	{Node<int>* ch;	ch = new Node<int>;	ch->data = 12;	obj.Insert(ch, &obj.root);	obj.R_H();	cout << "\n-------------------------------------\n"; obj.display(obj.root); }
 	
 	
-	//{Node<int>* ch;	ch = new Node<int>;	ch->data = 11;	obj.Insert(ch, &obj.root);	obj.R_H();	cout << "\n-------------------------------------\n"; obj.display(obj.root); }
-	//{Node<int>* ch;	ch = new Node<int>;	ch->data = 10;	obj.Insert(ch, &obj.root);	obj.R_H();	cout << "\n-------------------------------------\n"; obj.display(obj.root); }
-	//{Node<int>* ch;	ch = new Node<int>;	ch->data = 8;	obj.Insert(ch, &obj.root);	obj.R_H();	cout << "\n-------------------------------------\n"; obj.display(obj.root); }
-
 
 
 	cout << "\n\n\n";
