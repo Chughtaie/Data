77b3f1915a1593a4fcd414b61ba66831ad2ce6ed
Chughtae <Chughtae@github.com>
2020-12-14 22:31:23 +0500

    Update Header.h

--- Header.h
+++ Header.h
@@ -2,40 +2,160 @@
 //#include <iostream>
 //using namespace std;
 
-template<typename Type>
-class circular_List {
-	template<typename Type>
-	struct Node {
+struct Node {
 
-		AvlTree<Type> tree;
-		Node* next = NULL;
+	string data;
+	Node* next;
+	Node* prev;
+
+	Node() { next = prev = NULL; data = ""; }
+};
+
+
+class Table {
+
+
+
+
+
+public:
+
+	Node* head;
+	int count;
+
+	Table() { head =  NULL; count = 0; }
+
+	void insert(Node* n)
+	{
+		if (!head)
+		{
+			//head = new Node;
+			head = n;
+			//head = new Node;
+			//head->next = head;
+			//head->prev = head;
+			// head->prev = head->
+			//cout <<"insertion 1 " <<head->node->data << endl;
+			//head->next = head; count++;
+		}
+		else
+		{
+			Node* temp = head;
+
+			while (temp->next != NULL)
+			{
+				temp = temp->next;
+			}
+
+			//temp->next = new Node;
+			temp->next = n;
+			temp->next->prev = temp;
+			//cout << "insertion 2 " << temp->next->node->data << endl;
+			//temp->next->next = head; count++;
+
+		}
+		count++;
+	}
+
+	void display()
+	{
+		Node* temp = head;
+		while (temp != NULL)
+		{
+			cout << temp->data << endl;
+			temp = temp->next;
+		}
+		//temp->tree.display(temp->tree.root);
+
+	}
+
+
+	bool Delete(Node* n) {
+	
+		Node* temp = head;
+		Node* temp1;
+
+		if (head->data == n->data)
+		{
+			temp1 = head;
+			head = head->next;
+			count--;
+			temp1 = NULL;
+			return true;
+		}
+		else 
+		while (temp != NULL)
+		{
+			if (temp->data == n->data)
+			{	
+				temp1 = temp->prev;
+				temp1->next = temp->next;
+				if(temp->next)
+				temp->next->prev = temp1;
+				count--;
+				temp = NULL;
+				return true;
+			}
+			temp = temp->next;
+		}
+		return false;
+	}
+
+
+	int getcount() {
+
+		return count;
+	}
+
+};
+
+
+
+
+//================ RING ====================
+
+
+template<typename Type>
+class Ring {
+
+	template<typename Type>
+	struct Machine {
+		string path;
+		string value;
+		string key;
+
+		Table Fht;
+		AvlTree<Type> Tree;
+		Machine* next = NULL;
+
+		Machine() { path = ""; value = ""; key = ""; }
 	};
 
 
-	Node<Type>* head;
+	Machine<Type>* head;
 	int count;
 public:
 
-	circular_List() { head = NULL; count = 0; }
+	Ring() { head = NULL; count = 0; }
 
 	void insert(AvlTree<Type> n)
 	{
 		if (!head)
 		{
-			head = new Node<Type>;
+			head = new Machine<Type>;
 			head->tree = n;
 			//cout <<"insertion 1 " <<head->node->data << endl;
 			head->next = head; count++;
 		}
 		else
 		{
-			Node<Type>* temp = head;
+			Machine<Type>* temp = head;
 			while (temp->next != head)
 			{
 				temp = temp->next;
 			}
 
-			temp->next = new Node<Type>;
+			temp->next = new Machine<Type>;
 			temp->next->tree = n;
 			//cout << "insertion 2 " << temp->next->node->data << endl;
 			temp->next->next = head; count++;
@@ -46,7 +166,7 @@ public:
 
 	void display()
 	{
-		Node<Type>* temp = head;
+		Machine<Type>* temp = head;
 		while (temp->next != head)
 		{
 			temp->tree.display(temp->tree.root);
